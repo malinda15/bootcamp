7f46c9b78f6a86359d5f1a600659f6581d4c887a
malinda15 <50108117+malinda15@users.noreply.github.com>
2019-04-29 21:40:56 +0700

    Add files via upload

---
+++ test1.php
@@ -0,0 +1,13 @@
+<?php
+for($i=10; $i>=1; $i--){
+
+for($j=1; $j<=$i; $j++){
+
+//echo "&nbsp;&nbsp;";
+
+//for($k=1; $k<=$i; $k++){
+echo "$j";
+}
+echo "<br>";
+}
+?>
---
+++ test2.php
@@ -0,0 +1,13 @@
+<?php
+for($i=1; $i<=5; $i++){
+
+for($j=$i; $j>=1; $j--){
+
+echo "&nbsp;&nbsp;";
+
+//for($k=$j; $k>=1; $k++){
+echo "$j";
+}
+echo "<br>";
+}
+?>
